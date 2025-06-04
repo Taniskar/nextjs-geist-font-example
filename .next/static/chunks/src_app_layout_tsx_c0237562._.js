c9f84bcd3b02e29d88c305f3ed549d8acf8bdbca
Taniskar Batt <79400620+Taniskar@users.noreply.github.com>
2025-06-04 14:51:45 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749028944357

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_69e3a47e._.js",
+  "static/chunks/[root-of-the-server]__32f8957e._.js",
+  "static/chunks/[root-of-the-server]__dc687cf0._.css"
+],
+    source: "dynamic"
+});
